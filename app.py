0e657f273ff59b4b5c7ae1cacabf6a532d51ff28
bradders2006 <jbr207@exeter.ac.uk>
2026-01-20 16:32:57 +0000

    Send Registration form data to app.py

--- app.py
+++ app.py
@@ -2,9 +2,22 @@ from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def register():
+@app.route("/")
+def index():
     return render_template("register.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    email = request.form["email"]
+    dob = request.form["dob"]
+    password = request.form["password"]
+    repeat_password = request.form["repeat_password"]
+
+    print(first_name, last_name, email, dob, password, repeat_password)
+
+    return "Registration Complete"
 
 if __name__ == '__main__':
     app.run(debug=True)
