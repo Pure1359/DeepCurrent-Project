534594112931d58f71929f65b7ea766c0bbd6360
Pure1359 <cn472@exeter.ac.uk>
2026-02-05 10:52:49 +0000

    make create_challenge, join_challenge_individual and join_challenge_group

--- app/services/challenges.py
+++ app/services/challenges.py
@@ -1,4 +1,6 @@
 from app.db_config import db_cursor
+from app.services.auth import verify_session_role
+from flask import abort
 # Placeholder for now
 # Create functions that have to do with challenges
 # Follow templates in users.py and auth.py
@@ -8,16 +10,28 @@ from app.db_config import db_cursor
 # join_challenge_group
 # challenge_leaderboard_individual
 # challenge_leaderboard_group
-
 #Required role : Who can create the challenge? Parameter : Role -> {Admin, Locally Group Leader , etc}
-def create_challenge(role):
-    pass
+def create_challenge(created_by, challenge_type, is_official, title, start_date, end_date, rules, session_role):
+    if verify_session_role(session_role, "moderator"):
+        sql = """INSERT INTO Challenge (created_by, challenge_type, is_official, title, start_date, end_date, rules) 
+              VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+        with db_cursor as (connection, cursor):
+            cursor.execute(sql, (created_by, challenge_type, is_official, title, start_date, end_date, rules))
+    else:
+        abort(404)
+#Add user and challenge to the individualParticipation table
+def join_challenge_individual(challenge_id, account_id, join_date):
+    sql = """INSERT INTO IndividualParticipation (challenge_id, account_id, join_date) VALUES(%s, %s, %s)"""
 
-def join_challenge_individual():
-    pass
+    with db_cursor() as (connection, cursor):
+        cursor.execute(sql, (challenge_id, account_id, join_date))
+    
+#Add group and challenge to the GroupParticipation table
+def join_challenge_group(challenge_id, group_id, join_date):
+    sql = """INSERT INTO GroupParticipation (challenge_id, group_id, join_date) VALUES(%s, %s, %s)"""
 
-def join_challenge_group():
-    pass
+    with db_cursor() as (connection, cursor):
+        cursor.execute(sql, (challenge_id, group_id, join_date))
 
 def challenge_leaderboard_individual():
     pass
