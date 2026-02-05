bb8076faf14a3c9e15eec2c822396c89c8de0eb3
Pure1359 <cn472@exeter.ac.uk>
2026-02-05 12:10:25 +0000

    view_submission_list , make_submission_decision

--- app/moderator.py
+++ app/moderator.py
@@ -17,9 +17,6 @@ def is_moderator():
     else:
         pass
 
-    
-
-
 @moderator_bp.route("/view_submission_list", methods = ["GET", "POST"])
 def view_submission_list():
     offset = request.args.get("offset", 0)
@@ -33,7 +30,7 @@ def view_submission_list():
     except ValueError:
         abort(400, description = "offset or limit data type must be int")
 
-    sql = """SELECT evidence_type, evidence_url, evidence_date
+    sql = """SELECT evidence_id, evidence_type, evidence_url, evidence_date
              FROM Evidence
              ORDER BY evidence_date DESC
              LIMIT %s
@@ -46,21 +43,21 @@ def view_submission_list():
 
         json_list = []
         for record_tuple in submission_list:
-            new_dict = {"evidence_type" : record_tuple[0], "evidence_url" : record_tuple[1], "evidence_date" : record_tuple[2]}
+            new_dict = {"evidence_id" : record_tuple[0], "evidence_type" : record_tuple[1], "evidence_url" : record_tuple[2], "evidence_date" : record_tuple[3]}
             json_list.append(new_dict)
         
         return jsonify(json_list)
 
-        
-    
+@moderator_bp.route("/approve_submission<evidence_id><decision><reason>/")
+def make_submission_decision(evidence_id, decision, reason):
+    sql = """INSERT INTO Decision(evidence_id, reviewer_id, status, timestamp, reason) VALUES (%s, %s, %s, %s, %s)"""
 
-@moderator_bp.route("/view_submission<id>")
-def view_specific_submission(id):
-    
+    now = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+    reviewer_id = session.get("account_id")
 
-@moderator_bp.route("/view_submission<id><decision>/")
-def approved_specific_submission(id, decision):
-    pass
+    with db_cursor() as (connection, cursor):
+        cursor.execute(sql, (evidence_id, reviewer_id, decision, now, reason))
+    
 
 
 
