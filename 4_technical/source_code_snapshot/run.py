59309a0e99dd327438be58560d25d659ffcb46cb
Pure1359 <cn472@exeter.ac.uk>
2026-02-10 20:39:18 +0000

    change env reading location

--- 4_technical/source_code_snapshot/run.py
+++ 4_technical/source_code_snapshot/run.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
 from app import create_app
-
+import os
 # Load environment variables from .env file
-load_dotenv("secret.env")
+load_dotenv("DeepCurrent-Project\secret.env")
 
 app = create_app()
 
 if __name__ == '__main__':
+    print(os.getenv("MYSQL_HOST"))
     app.run(host="localhost", port=5000, debug=True)
