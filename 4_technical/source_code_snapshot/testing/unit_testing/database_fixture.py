c1451a74da4ddae18ae613865c95ae5d01758fca
Pure1359 <cn472@exeter.ac.uk>
2026-02-11 12:25:00 +0000

    implement setup and tear down for sqlite (may update more to optimize performance)

--- 4_technical/source_code_snapshot/testing/unit_testing/database_fixture.py
+++ 4_technical/source_code_snapshot/testing/unit_testing/database_fixture.py
@@ -4,17 +4,28 @@ import sqlite3
 @pytest.fixture
 def function_scope_database():
     #set up connection
-    conn = sqlite3.connect('mydb.sqlite')
+    database_path = '4_technical/source_code_snapshot/sqlite_database/mydb.sqlite'
+    schema_path  = '4_technical/source_code_snapshot/sqlite_database/schema.sql'
+    conn = sqlite3.connect(database_path)
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
+    #turn off the foreign key to make dropping table easier and get all table name
+    cursor.execute("PRAGMA foreign_keys = OFF")
     cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table'")
 
+    #for all table name , drop entire table structure
     for schema in cursor.fetchall():
         table_name = schema['name']
-
-        sql = f"DELETE FROM {table_name}"
+        sql = f"DROP TABLE IF EXISTS {table_name}"
         cursor.execute(sql)
     
+    conn.commit()
+
+    #recreate table and enable the foreign key again
+    with open(schema_path) as create_script:
+        table_schema = create_script.read()
+    cursor.executescript(table_schema)
+    cursor.execute("PRAGMA foreign_keys = ON")
     conn.commit()
 
     #return connection
