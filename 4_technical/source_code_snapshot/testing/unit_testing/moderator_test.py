315a762610d660f8b0dcbfa0db8e123c35fee321
Pure1359 <cn472@exeter.ac.uk>
2026-02-13 15:33:45 +0000

    test moderator create challenge

--- 4_technical/source_code_snapshot/testing/unit_testing/moderator_test.py
+++ 4_technical/source_code_snapshot/testing/unit_testing/moderator_test.py
@@ -18,11 +18,13 @@ def test_login_moderator(new_client_module, recorded_template_module, module_sco
         assert session.get("account_role") == "moderator"
 
 def test_moderator_make_challenge(new_client_module, recorded_template_module, module_scope_database):
+    start_date = datetime.now()
+    end_date = datetime.now() + timedelta(days = 30)
     response = new_client_module.post("/moderator_access/create_challenge", data = {
         "challenge_type" : "travel",
         "title" : "walk alot",
-        "start_date" : datetime.now(),
-        "end_date" : datetime.now() + timedelta(days = 30),
+        "start_date" : start_date,
+        "end_date" : end_date,
         "rule" : "walk as much as you can"
     })
     response = response.get_json()
@@ -31,8 +33,18 @@ def test_moderator_make_challenge(new_client_module, recorded_template_module, m
     with db_cursor() as (connection, cursor):
         sql = """SELECT * FROM Challenge WHERE challenge_id = %s"""
         cursor.execute(sql, (challenge_id,))
+        response = cursor.fetchone()
 
-        assert cursor.fetchall() is not None
+        assert response["created_by"] == 2
+        assert response["challenge_type"] == "travel"
+        assert response["title"] == "walk alot"
+        assert response["start_date"] == str(start_date)
+        assert response["end_date"] == str(end_date)
+        assert response["rules"] == "walk as much as you can"
 
-
-    
+        sql = "SELECT * FROM Accounts"
+        cursor.execute(sql)
+        accounts = cursor.fetchall()
+        
+        for account in accounts:
+            print(account)
