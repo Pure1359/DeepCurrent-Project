96d8c1d6f04202a9531107111e01661ad4f53dea
Pure1359 <cn472@exeter.ac.uk>
2026-02-13 17:33:22 +0000

    more moderator test, viewing submission and making challenge

--- 4_technical/source_code_snapshot/testing/unit_testing/moderator_test.py
+++ 4_technical/source_code_snapshot/testing/unit_testing/moderator_test.py
@@ -22,10 +22,10 @@ def test_moderator_make_challenge(new_client_module, recorded_template_module, m
     end_date = datetime.now() + timedelta(days = 30)
     response = new_client_module.post("/moderator_access/create_challenge", data = {
         "challenge_type" : "travel",
-        "title" : "walk alot",
+        "title" : "Let Walk",
         "start_date" : start_date,
         "end_date" : end_date,
-        "rule" : "walk as much as you can"
+        "rule" : "Walk in a park"
     })
     response = response.get_json()
     challenge_id = response["challenge_id"]
@@ -37,14 +37,21 @@ def test_moderator_make_challenge(new_client_module, recorded_template_module, m
 
         assert response["created_by"] == 2
         assert response["challenge_type"] == "travel"
-        assert response["title"] == "walk alot"
+        assert response["title"] == "Let Walk"
         assert response["start_date"] == str(start_date)
         assert response["end_date"] == str(end_date)
-        assert response["rules"] == "walk as much as you can"
+        assert response["rules"] == "Walk in a park"
 
         sql = "SELECT * FROM Accounts"
         cursor.execute(sql)
         accounts = cursor.fetchall()
-        
-        for account in accounts:
-            print(account)
+
+def test_moderator_view_pending_evidence(new_client_module, recorded_template_module, module_scope_database):
+    response = new_client_module.post("/moderator_access/view_submission_list", json = {
+        "offset" : 0,
+        "limit" : 100
+    }, follow_redirects = True)
+    response = response.get_json()
+    assert len(response) == 1
+    
+
