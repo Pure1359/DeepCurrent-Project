1016300ead7a3288956f0839b7789dd8275c6f10
Carlin Barlow <cbarlow6c@gmail.com>
2026-03-23 20:18:18 +0000

    seeded_data.py Bux Fixes

--- 4_technical/source_code_snapshot/run.py
+++ 4_technical/source_code_snapshot/run.py
@@ -6,10 +6,8 @@ from set_up.database_setup import *
 load_dotenv("secret.env")
 
 app = create_app()
-deleterecord()
 default_actionType_data()
 defaultDatabase()
-production_setup()
 
 
 if __name__ == '__main__':
